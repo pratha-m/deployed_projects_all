f1726229f0a4f0df788963eeb2a9d6041905b7a5
pratha-m <prathamchhabra85@gmail.com>
2023-08-22 11:08:10 +0530

    cors updated..

--- React Web Apps/deploy ecommerce mern app/server/index.js
+++ React Web Apps/deploy ecommerce mern app/server/index.js
@@ -24,7 +24,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 app.use(cors({
     credentials:true,
-    origin: ['https://ecommercemernapp.netlify.app/']
+    origin: ['https://ecommercemernapp.netlify.app']
 }))
 app.use(cookieParser())
 

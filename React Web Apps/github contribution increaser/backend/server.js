54b14c9aba6756e16b161ffc6032e8f3f85222af
pratha-m <prathamchhabra85@gmail.com>
2024-01-16 20:57:30 +0530

    set scheuleer

--- React Web Apps/github contribution increaser/backend/server.js
+++ React Web Apps/github contribution increaser/backend/server.js
@@ -6,6 +6,8 @@ const { sendEmail } = require("./helper/sendEmail");
 
 const app=express();
 
+// 0 0 * * * --> run at 00:00 (every 24 hour)
+
 cron.schedule("0 * * * *",()=>{
    console.log("Running after 1 hour");
    sendEmail(process.env.TO_EMAIL);
